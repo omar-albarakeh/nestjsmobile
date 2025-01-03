d6dbbe219bf1d711c4ba20ed985e15aa402fa937
omar albarakeh <omaralbarakeh2@gmail.com>
2025-01-03 02:36:00 +0200

    NestJS <> jwtstrategy updated

---
+++ src/Auth/jwt.strategy.ts
@@ -0,0 +1,30 @@
+import { Injectable, UnauthorizedException } from '@nestjs/common';
+import { InjectModel } from '@nestjs/mongoose';
+import { Model } from 'mongoose';
+import { Strategy, ExtractJwt } from 'passport-jwt';
+import { User } from './schemas/user.schema';
+import { PassportStrategy } from '@nestjs/passport';
+import { ConfigService } from '@nestjs/config';
+
+@Injectable()
+export class JwtStrategy extends PassportStrategy(Strategy) {
+  constructor(
+    @InjectModel(User.name) private readonly userModel: Model<User>,
+    private readonly configService: ConfigService,
+  ) {
+    super({
+      jwtFromRequest: ExtractJwt.fromAuthHeaderAsBearerToken(),
+      secretOrKey: configService.get<string>('JWT_SECRET'),
+    });
+  }
+
+  async validate(payload: { id: string }) {
+    const user = await this.userModel.findById(payload.id);
+
+    if (!user) {
+      throw new UnauthorizedException('Login first to access this endpoint.');
+    }
+
+    return user;
+  }
+}
