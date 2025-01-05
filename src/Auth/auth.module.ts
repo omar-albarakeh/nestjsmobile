30dff4a61c6abee44210ac6bd781ae6354a71734
omar albarakeh <omaralbarakeh2@gmail.com>
2025-01-05 16:06:32 +0200

    Nestjs <> updated

--- src/Auth/auth.module.ts
+++ src/Auth/auth.module.ts
@@ -2,6 +2,7 @@ import { Module } from '@nestjs/common';
 import { JwtModule } from '@nestjs/jwt';
 import { PassportModule } from '@nestjs/passport';
 import { MongooseModule } from '@nestjs/mongoose';
+import { ConfigService } from '@nestjs/config';
 
 import { AuthController } from './auth.controller';
 import { AuthService } from './auth.service';
@@ -12,10 +13,14 @@ import { User, UserSchema } from './schemas/user.schema';
 @Module({
   imports: [
     PassportModule.register({ defaultStrategy: 'jwt' }),
-    JwtModule.register({
-      secret: process.env.JWT_SECRET,
-      signOptions: { expiresIn: process.env.JWT_EXPIRES || '1h' },
-    }),
+    JwtModule.registerAsync({
+  inject: [ConfigService],
+  useFactory: (configService: ConfigService) => ({
+    secret: configService.get<string>('JWT_SECRET'),
+    signOptions: { expiresIn: configService.get<string>('JWT_EXPIRES') || '1h' },
+  }),
+}),
+
     MongooseModule.forFeature([{ name: User.name, schema: UserSchema }]),
   ],
   controllers: [AuthController],
