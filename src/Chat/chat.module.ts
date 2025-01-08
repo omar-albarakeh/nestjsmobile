411b88fc1bb6d6189498fb27a4fb2abdc6413951
omar albarakeh <omaralbarakeh2@gmail.com>
2025-01-08 16:10:02 +0200

    chat module updated

--- src/Chat/chat.module.ts
+++ src/Chat/chat.module.ts
@@ -1,9 +1,11 @@
+// src/chat/chat.module.ts
 import { Module } from '@nestjs/common';
 import { MongooseModule } from '@nestjs/mongoose';
 import { ChatService } from './chat.service';
 import { ChatController } from './chat.controller';
 import { MessageSchema } from './message.schema';
 import { ChatSchema } from './chat.schema';
+import { ChatGateway } from './chat.gateway';
 
 @Module({
   imports: [
@@ -12,7 +14,7 @@ import { ChatSchema } from './chat.schema';
       { name: 'Chat', schema: ChatSchema },
     ]),
   ],
-  providers: [ChatService],
+  providers: [ChatService, ChatGateway], 
   controllers: [ChatController],
 })
 export class ChatModule {}
