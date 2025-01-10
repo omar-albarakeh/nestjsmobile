85606854d5d637ca069ea78059a5f3ad3b365b84
omar albarakeh <omaralbarakeh2@gmail.com>
2025-01-10 22:42:09 +0200

    Nestjs <> new dto added

---
+++ src/market/dto/createmarketdto.ts
@@ -0,0 +1,27 @@
+import { IsNotEmpty, IsString, IsNumber, IsOptional } from 'class-validator';
+
+export class CreateMarketItemDto {
+  @IsNotEmpty()
+  @IsString()
+  name: string;
+
+  @IsNotEmpty()
+  @IsNumber()
+  price: number;
+
+  @IsNotEmpty()
+  @IsString()
+  category: string;
+
+  @IsNotEmpty()
+  @IsString()
+  description: string;
+
+  @IsNotEmpty()
+  @IsString()
+  imageUrl: string;
+
+  @IsNotEmpty()
+  @IsNumber()
+  stockQuantity: number;
+}
