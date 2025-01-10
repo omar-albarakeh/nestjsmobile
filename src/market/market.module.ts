96b7dfa3d94a52ef525a5148bf87c42516f333d9
omar albarakeh <omaralbarakeh2@gmail.com>
2025-01-10 21:55:31 +0200

    Nestjs <> market module added

---
+++ src/market/market.module.ts
@@ -0,0 +1,15 @@
+import { Module } from '@nestjs/common';
+import { MongooseModule } from '@nestjs/mongoose';
+import { MarketItemController } from './market-item.controller';
+import { MarketItemService } from './market-item.service';
+import { MarketItemRepository } from './market-item.repository';
+import { MarketItem, MarketItemSchema } from './market-item.schema';
+
+@Module({
+  imports: [
+    MongooseModule.forFeature([{ name: MarketItem.name, schema: MarketItemSchema }]),
+  ],
+  controllers: [MarketItemController],
+  providers: [MarketItemService, MarketItemRepository],
+})
+export class MarketModule {}
