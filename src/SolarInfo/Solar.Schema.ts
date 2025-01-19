46a3889e4d8bcb62806a22c810e5e8a497a41677
omar albarakeh <omaralbarakeh2@gmail.com>
2025-01-19 23:47:08 +0200

    Nestjs <> solar info updated

--- src/SolarInfo/Solar.Schema.ts
+++ src/SolarInfo/Solar.Schema.ts
@@ -1,18 +1,25 @@
 import { Prop, Schema, SchemaFactory } from '@nestjs/mongoose';
+import { Document } from 'mongoose';
 
 @Schema()
-export class SolarInfo {
-  @Prop({ required: true })
-  panelsNb: string;
+export class SolarInfo extends Document {
+  @Prop({ required: false })
+  panelsNb?: string;
 
-  @Prop({ required: true })
-  panelEfficiency: string;
+  @Prop({ required: false })
+  panelEfficiency?: string;
 
-  @Prop({ required: true })
-  panelWatt: string;
+  @Prop({ required: false })
+  panelWatt?: string;
 
-  @Prop({ required: true })
-  panelArea: string;
+  @Prop({ required: false })
+  panelArea?: string;
+
+  @Prop({ required: false })
+  inverterCapacity?: number;
+
+  @Prop({ required: false })
+  installationDate?: string;
 }
 
 export const SolarInfoSchema = SchemaFactory.createForClass(SolarInfo);
