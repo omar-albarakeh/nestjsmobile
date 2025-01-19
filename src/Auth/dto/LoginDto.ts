6054d4080b89ba7dc8c43608410b7f86c2bec706
omar albarakeh <omaralbarakeh2@gmail.com>
2025-01-19 23:44:00 +0200

    Nestjs <> login dto updated

--- src/Auth/dto/LoginDto.ts
+++ src/Auth/dto/LoginDto.ts
@@ -1,10 +1,4 @@
-import { 
-  IsEmail, 
-  IsNotEmpty, 
-  IsString, 
-  MinLength,  
-  Matches 
-} from 'class-validator';
+import { IsEmail, IsNotEmpty, IsString, MinLength, Matches } from 'class-validator';
 
 export class LoginDto {
   @IsNotEmpty({ message: 'Email is required' })
@@ -14,8 +8,9 @@ export class LoginDto {
   @IsNotEmpty({ message: 'Password is required' })
   @IsString()
   @MinLength(6, { message: 'Password must be at least 6 characters long' })
-  @Matches(/^(?=.*[A-Z])(?=.*\d)(?=.*[!@#$%^&*]).{6,}$/, { 
-    message: 'Password must contain at least one uppercase letter, one number, and one special character',
+  @Matches(/^(?=.*[A-Z])(?=.*\d)(?=.*[!@#$%^&*]).{6,}$/, {
+    message:
+      'Password must contain at least one uppercase letter, one number, and one special character',
   })
   readonly password: string;
 }
